ddc0abab161f77a46205c618e051f067ebb32cd5
jyuan4 <jyuan4@tigers.lsu.edu>
2014-10-06 17:03:32 -0500

    delete files

--- funcPlotImageLineProbe.m
+++
@@ -1,8 +0,0 @@
-function [g1 g2]=funcPlotImageLineProbe(image,dimension)
-subplot(1,2,1)
-g1=imresize(imadjust(imagesc(image)),256);
-colormap(gray)
-
-subplot(1,2,2)
-g2=plot(image(round(dimension/2),:));
-
